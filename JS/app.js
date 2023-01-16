c551588072e72a21ae6a5f93c8eec4309c9b0032
reedoooo <readvogt@gmail.com>
2023-01-16 13:15:22 -0700

    framework

---
+++ JS/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let instructor = {
+  name: 'Reed',
+  age: 23,
+  class: '201d96',
+  likeJavaScript: true,
+  teach: function () {
+    console.log('Javascript is great');
+  },
+};
+
+console.log(instructor);
+instructor.teach();
+let Seattle = {
+
+};
+
+let Tokyo = {
+
+};
+
+let Dubai = {
+
+};
+
+let Paris = {
+
+};
+
+let Lima = {
+
+};
